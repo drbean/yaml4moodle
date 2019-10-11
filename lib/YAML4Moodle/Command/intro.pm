e4488169f23f0864713a2c984e7c9aeedfd94087
Dr Bean <drbean@sac.nuu.edu.tw>
2019-10-11 09:55:16 +0800

    topic,story,form

--- lib/YAML4Moodle/Command/intro.pm
+++ lib/YAML4Moodle/Command/intro.pm
@@ -7,19 +7,16 @@ use strict;
 use warnings;
 use YAML qw/Dump LoadFile DumpFile/;
 use IO::All;
-use XML::DOM;
 
 
-sub abstract { "Create Moodle description quiz question in xml format on fly" }
-sub description { "Markdown text description inserted in multi-question quiz" }
+sub abstract { "Create file for Moodle intro text from cards.yaml" }
+sub description { "Frequent use transfering intro text from essay rubric in cards.yaml" }
 
-sub usage_desc { 'yaml4moodle description -t $markdown_text -i identifier' }
+sub usage_desc { 'yaml4moodle intro -t read -s vacation -f 0' }
 
 sub opt_spec  {
         return (
-                ["d=s", "questiontext"]
-                , ["i=s", "identifier"]
-                , ["t=s", "topic"]
+                ["t=s", "topic"]
                 , ["s=s", "story"]
                 , ["f=s", "form"]
 	);
@@ -29,53 +26,15 @@ sub opt_spec  {
 sub execute {
 	my ($self, $opt, $args) = @_;
 
-	my ($description, $identifier, $topic, $story, $form) = @$opt{qw/d i t s f/};
-	die "description questiontext: '$description'?" unless $description;
-	die "description identifier '$identifier'?" unless $identifier;
+	my ($topic, $story, $form) = @$opt{qw/t s f/};
 	die "description topic '$topic'?" unless $topic;
 	die "description story '$story'?" unless $story;
 	die "description form '$form'?" unless defined $form;
 
-	my $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-
-	$XML::DOM::Parser::KeepCDATA = 1;
-	my $q = XML::DOM::Document->createElement("quiz");
-	my $cat_qn = XML::DOM::Document->createElement("question");
-	$cat_qn->setAttribute("type","category");
-	my $cat = XML::DOM::Document->createElement("category");
-	my $cat_text = XML::DOM::Document->createElement("text");
-	$cat_text->addText("\$cat1\$/top/$topic/$story");
-	$cat->appendChild($cat_text);
-	$cat_qn->appendChild($cat);
-	$q->appendChild($cat_qn);
-
-	my $comment = XML::DOM::Document->createComment
-	("identifier: $identifier");
-	$q->appendChild( $comment );
-
-	my $prefix = substr $description, 0, 15;
-	my $qn = XML::DOM::Document->createElement("question");
-	$qn->setAttribute("type","description");
-	my $name = XML::DOM::Document->createElement("name");
-	my $text = XML::DOM::Document->createElement("text");
-	$text->addText("$story $form: $prefix description");
-	$name->appendChild( $text);
-	$qn->appendChild( $name);
-
-	my $qntext = XML::DOM::Document->createElement("questiontext");
-	$qntext->setAttribute("format","markdown");
-	$text = XML::DOM::Document->createElement("text");
-	my $cdata_text = $description;
-	$XML::DOM::Parser::KeepCDATA = 1;
-	my $cdata = XML::DOM::Document->createCDATASection( $cdata_text );
-	$text->appendChild( $cdata );
-	$qntext->appendChild( $text );
-	$qn->appendChild( $qntext );
-	$q->appendChild($qn);
-
-	$xml .= $q->toString;
-
-	$xml > io("-");
+	my $y = LoadFile "$topic/cards.yaml";
+	my $io = io "$topic/intro.txt";
+	$io->print( $y->{$story}->{essay}->{$form}->{rubric}  );
+	$io->autoflush;
 }
 
 1;
